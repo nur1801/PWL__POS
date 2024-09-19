6a1c2855a5b86944059ef1ef68f5b266256672b9
Nurhidayah <nurhid180701@gmail.com>
2024-09-20 06:27:23 +0700

    [JS04]Pratikum 1 - $fillable

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -19,12 +19,32 @@ class UserController extends Controller
         // ];
         // UserModel::insert($data);
 
-        $data = [
-            'nama' => 'Pelanggan Pertama',
-        ];
-        UserModel::where('username', 'customer-2')->update($data); 
+        // $data = [
+        //     'nama' => 'Pelanggan Pertama',
+        // ];
+        // UserModel::where('username', 'customer-2')->update($data); 
 
-        //coba akses model UserModel
+        // //coba akses model UserModel
+        // $user = UserModel::all(); //ambil semua data dari table m_user
+        // return view('user', ['data' => $user]);
+
+        // JS04 - Pratikum 1
+        // $data = [
+        //     'level_id' => '2',
+        //     'username' => 'manager_2',
+        //     'nama' => 'Manager 2',
+        //     'password' => Hash::make('12345'),
+        // ];
+        // UserModel::insert($data);
+
+        $data = [
+            'level_id' => '2',
+            'username' => 'manager_3',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345'),
+        ];
+        UserModel::insert($data);
+
         $user = UserModel::all(); //ambil semua data dari table m_user
         return view('user', ['data' => $user]);
     }
--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -11,4 +11,13 @@ class UserModel extends Model
 
     protected $table = 'm_user'; //Mendefinisikan nama table yang digunakan oleh model ini
     protected $primaryKey = 'user_id'; //Mendefinisikan primary key dari table yang digunakan
+
+    // JS04 - Praktikum 1
+     /**
+     * The attributes that are mass assignable
+     * 
+     * @var array
+     */
+    // protected $fillable = ['level_id', 'username', 'nama', 'password'];
+    protected $fillable = ['level_id', 'username', 'nama'];
 }
