f79a9e69d971206269580203ced7176cffd713c9
Nurhidayah <nurhid180701@gmail.com>
2024-09-20 11:15:02 +0700

    [JS04]Pratikum 2.6 - Create, Read, Update, Delete (CRUD)

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -44,7 +44,7 @@
             {{-- ------------------------------------------------ --}}
 
             {{-- JS 04 - Pratikum 2.4 - Retieving or Creating Models --}}
-            <tr>
+            {{-- <tr>
                 <td>ID</td>
                 <td>Username</td>
                 <td>Nama</td>
@@ -55,7 +55,31 @@
                 <td>{{ $data ->username }}</td>
                 <td>{{ $data ->nama }}</td>
                 <td>{{ $data ->level_id }}</td>
-            </tr>
+            </tr> --}}
+            {{-- ---------------------------------------------------- --}}
+
+            {{-- JS 04 - Pratikum 2.6 - Create, Read, Update, Delete (CRUD) --}}
+            <body>
+                <a href="{{url('/user/tambah')}}">+ Tambah User</a>
+                <table border="1" cellpadding="2" cellspacing="0">
+                    <tr>
+                        <td>ID</td>
+                        <td>Username</td>
+                        <td>Nama</td>
+                        <td>ID Level Pengguna</td>
+                        <td>Aksi</td>
+                    </tr>
+                    @foreach ($data as $d)
+                    <tr>
+                        <td>{{ $d->user_id }}</td>
+                        <td>{{ $d->username }}</td>
+                        <td>{{ $d->nama }}</td>
+                        <td>{{ $d->level_id }}</td>
+                        <td><a href="{{ url('/user/ubah/' . $d->user_id) }}">Ubah</a> | <a href="{{ url('/user/hapus/' . $d->user_id) }}">Hapus</a></td>
+                    </tr>
+                    @endforeach
+                </table>
+            </body>
         </table>
     </body>
 </html>
