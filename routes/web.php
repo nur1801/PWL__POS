aa120c907dcb6d17e4f13d632f57dbdf119b1420
Nurhidayah <nurhid180701@gmail.com>
2024-10-11 09:38:01 +0700

    [JS07]Tugas 4 - Implementasi Form Registrasi

--- routes/web.php
+++ routes/web.php
@@ -156,6 +156,8 @@ use App\Http\Controllers\AuthController;
 Route::pattern('id', '[0-9]+');
 Route::get('login', [AuthController::class, 'login'])->name('login');
 Route::post('login', [AuthController::class, 'postlogin']);
+Route::get('register', [AuthController::class, 'register']);
+Route::post('register', [AuthController::class, 'postRegister']);
 Route::get('logout', [AuthController::class, 'logout'])->middleware('auth');
 
 Route::middleware('auth')->group(function () {
