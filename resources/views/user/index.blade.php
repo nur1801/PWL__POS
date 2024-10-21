07c71bd99cfcf4cb67fcd434a445bb6c03946b36
Nurhidayah <nurhid180701@gmail.com>
2024-10-21 19:36:52 +0700

    Modifikasi Fungsi Filter (Data User)

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -6,18 +6,16 @@
             <h3 class="card-title">{{ $page->title }}</h3>
             <div class="card-tools">
                 <a class="btn btn-sm btn-primary mt-1" h href="{{ url('/user/export_excel') }}" class="btn btn-primary"><i
-                    class="fa fa-file-excel"></i> Export User</a>
-                    <a class="btn btn-sm btn-warning mt-1" href="{{ url('/user/export_pdf') }}" class="btn btn-warning"><i
+                        class="fa fa-file-excel"></i> Export User</a>
+                <a class="btn btn-sm btn-warning mt-1" href="{{ url('/user/export_pdf') }}" class="btn btn-warning"><i
                         class="fa fa-file-pdf"></i> Export User</a>
-                <button onclick="modalAction('{{ url('user/import') }}')" class="btn btn-sm btn-info mt-1">Import
+                <button onclick="modalAction('{{ url('/user/import') }}')" class="btn btn-sm btn-info mt-1">Import
                     User</button>
-                {{-- [JS06] Pratikum 1 - Modal Ajax Tambah Data (Data User) --}}
-                <button onclick="modalAction('{{ url('user/create_ajax') }}')" class="btn btn-sm btn-success mt-1">Tambah
-                    ajax</button>
+                <button onclick="modalAction('{{ url('/user/create_ajax') }}')" class="btn btn-sm btn-success mt-1">Tambah
+                    Ajax</button>
             </div>
         </div>
         <div class="card-body">
-            {{-- menambahkan tampilan jika ada pesan error --}}
             @if (session('success'))
                 <div class="alert alert-success">{{ session('success') }}</div>
             @endif
@@ -27,9 +25,9 @@
             <div class="row">
                 <div class="col-md-12">
                     <div class="form-group row">
-                        <label class="col-1 control-label col-form-label">Filter </label>
+                        <label class="col-1 control-label col-form-label">Filter:</label>
                         <div class="col-3">
-                            <select class="form-control" id="leve_id" name="leve_id" required>
+                            <select class="form-control" id="level_id" name="level_id" required>
                                 <option value="">- Semua -</option>
                                 @foreach ($level as $item)
                                     <option value="{{ $item->level_id }}">{{ $item->level_nama }}</option>
@@ -40,7 +38,7 @@
                     </div>
                 </div>
             </div>
-            <div class="table-responsive">
+            <div class= "table-responsive">
                 <table class="table table-bordered table-striped table-hover table-sm" id="table_user">
                     <thead>
                         <tr>
@@ -53,9 +51,9 @@
                     </thead>
                 </table>
             </div>
+        </div>
     </div>
-    {{-- [JS06] Pratikum 1 - Modal Ajax Tambah Data (Data User) --}}
-    <div id="myModal" class="modal fade animate shake" tabindex="-1" role="dialog" databackdrop="static"
+    <div id="myModal" class="modal fade animate shake" tabindex="-1" role="dialog" data-backdrop="static"
         data-keyboard="false" data-width="75%" aria-hidden="true"></div>
 @endsection
 
@@ -64,17 +62,15 @@
 
 @push('js')
     <script>
-        // [JS06] Pratikum 1 - Modal Ajax Tambah Data (Data User)
         function modalAction(url = '') {
             $('#myModal').load(url, function() {
                 $('#myModal').modal('show');
             });
         }
-
-        var dataUser;
+        var datauser;
         $(document).ready(function() {
             dataUser = $('#table_user').DataTable({
-                // serverSide: true, jika ingin menggunakan server side processing
+                // serverSide: true, jika ingin menggunakan server side proses 
                 serverSide: true,
                 ajax: {
                     "url": "{{ url('user/list') }}",
@@ -85,46 +81,39 @@
                     }
                 },
                 columns: [{
-                        // nomor urut dari laravel datatable addIndexColumn()
-                        data: "DT_RowIndex",
-                        className: "text-center",
-                        orderable: false,
-                        searchable: false
-                    },
-                    {
-                        data: "username",
-                        className: "",
-                        // orderalbel: true, jika ingin kolom ini bisa diurutkan
-                        orderable: true,
-                        // searchable: true, jika ingin kolom ini bisa dicari
-                        searchable: true
-                    },
-                    {
-                        data: "nama",
-                        className: "",
-                        orderable: true,
-                        searchable: true
-                    },
-                    {
-                        // mengambil data level hasil dari ORM berelasi
-                        data: "level.level_nama",
-                        className: "",
-                        orderable: false,
-                        searchable: false
-                    },
-                    {
-                        data: "aksi",
-                        className: "",
-                        orderable: false, // orderalbel: true, jika ingin kolom ini bisa diurutkan
-                        searchable: false // searchable: true, jika ingin kolom ini bisa dicari
-                    }
-                ]
+                    // nomor urut dari laravel datatable addIndexColumn() 
+                    data: "DT_RowIndex",
+                    className: "text-center",
+                    orderable: false,
+                    searchable: false
+                }, {
+                    data: "username",
+                    className: "",
+                    // orderable: true, jika ingin kolom ini bisa diurutkan  
+                    orderable: true,
+                    // searchable: true, jika ingin kolom ini bisa dicari 
+                    searchable: true
+                }, {
+                    data: "nama",
+                    className: "",
+                    orderable: true,
+                    searchable: true
+                }, {
+                    // mengambil data level hasil dari ORM berelasi 
+                    data: "level.level_nama",
+                    className: "",
+                    orderable: false,
+                    searchable: false
+                }, {
+                    data: "aksi",
+                    className: "",
+                    orderable: false,
+                    searchable: false
+                }]
             });
-
             $('#level_id').on('change', function() {
                 dataUser.ajax.reload();
             });
-
         });
     </script>
 @endpush
