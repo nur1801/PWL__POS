4ec1f2d416826e2cb663f91f195e6cc92b76179c
Nurhidayah <nurhid180701@gmail.com>
2024-10-31 21:11:34 +0700

    [JS10]Praktikum 1 – Membuat RESTful API register

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 // [JS07] Pratikum 1 - Implementasi Authentication
 use Illuminate\Foundation\Auth\User as Authenticatable; // implementasi class Authenticatable
+// [JS10] Pratikum 1 - Membuat RESTful API Register
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
 // class UserModel extends Model
 // {
@@ -29,46 +31,55 @@ use Illuminate\Foundation\Auth\User as Authenticatable; // implementasi class Au
 //     }
 
 // [JS07] Pratikum 1 - Implementasi Authentication
-class UserModel extends Authenticatable
+class UserModel extends Authenticatable implements JWTSubject
 {
-    use HasFactory;
+  public function getJWTIdentifier()
+  {
+    return $this->getKey();
+  }
 
-    protected $table = 'm_user';        // Mendefinisikan nama tabel yang digunakan oleh model ini
-    protected $primaryKey = 'user_id';  // Mendfinisikan primary key dari tabel yang digunakan
-    protected $fillable = ['username', 'password', 'nama', 'level_id','created_at', 'updated_at'];
-    protected $hidden = ['password']; // jangan di tampilkan saat select
-    protected $casts = ['password' => 'hashed']; // casting password agar otomatis dihash
+  public function getJWTCustomClaims()
+  {
+    return [];
+  }
+  use HasFactory;
+
+  protected $table = 'm_user';        // Mendefinisikan nama tabel yang digunakan oleh model ini
+  protected $primaryKey = 'user_id';  // Mendfinisikan primary key dari tabel yang digunakan
+  protected $fillable = ['username', 'password', 'nama', 'level_id', 'created_at', 'updated_at'];
+  protected $hidden = ['password']; // jangan di tampilkan saat select
+  protected $casts = ['password' => 'hashed']; // casting password agar otomatis dihash
 
 
-    /**
-     * relasi ke table level
-     */
-    public function level() : BelongsTo
-    {
-        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
-    }   
+  /**
+   * relasi ke table level
+   */
+  public function level(): BelongsTo
+  {
+    return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+  }
 
-    /**
-     * mendapatkan nama role
-     */
-    public function getRoleName(): string
-    {
-      return $this->level->level_nama;
-    }
+  /**
+   * mendapatkan nama role
+   */
+  public function getRoleName(): string
+  {
+    return $this->level->level_nama;
+  }
 
-    /**
-     * cek apakah user memiliki role tertentu
-     */
-    public function hasRole($role): bool
-    {
-      return $this->level->level_kode == $role;
-    }
+  /**
+   * cek apakah user memiliki role tertentu
+   */
+  public function hasRole($role): bool
+  {
+    return $this->level->level_kode == $role;
+  }
 
-    /**
-     * Mendapatkan kode role
-     */
-    public function getRole()
-    {
-      return $this->level->level_kode;
-    }
+  /**
+   * Mendapatkan kode role
+   */
+  public function getRole()
+  {
+    return $this->level->level_kode;
+  }
 }
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+// [JS10] Pratikum 1 - Membuat RESTful API Register
+Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
